1020aa0efeb84b4100b24fe88aae7c966dc43433
Aly Farag <42911937+Alyfaraj@users.noreply.github.com>
2020-04-09 20:00:15 +0200

    Update env.js

--- env.js
+++ env.js
@@ -1,13 +1,13 @@
 export const dialogflowConfig ={
-    "type": "service_account",
-    "project_id": "solveit-onjhqn",
-    "private_key_id": "4c7b08ac9856bd8f05e4dd8410d3c2dcaa0f384b",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCfeLihmySqaPOa\nRNffQT2mFaOAHJTA5fGOwZHElf5KMZP0BGEoDbavyU7VmRUI18RVweWBtQ+B80ZC\noBO4tq1Gzj+Q/RSpv/QWeMciRMeca1L2ZNPanl8w2+zD+0ApnZR5bfcqUi2Oi825\nICg9RhnxBuqgNVzd4jrLdpO3jgc2/VDszhz0z15FLQaKLOKdhWQJib9XAKU0RtMf\nqlvH7u0RLYhqBPe9h/sWBtEsKBgCf2HMKDT8dC+AZSaymPLhUynGTMrNEgqyTdxw\nXizc//XGyTYn4U7T9Y0cpBa1XGpyBPXilHfeC7RBJeE0Ow1JhV2Xb3sTrCgA3gN1\nXlEjMG0jAgMBAAECggEACTbrCDTUOLU0Gj5kcNsutOmRgdC6TFkdDOrPbES0ev9Z\nYA4JZSHsY++QYBQ73Gpx8aNGCn7L+hjJ/wXhOIOfCIqDWXKI3p7KUQAX6uQphTbI\nW5dg4cQpQO6m7tNrljFWR8acXl7S5hEwSRmmvupNi8P4C7DPAGfW44nNqEYLJwNY\nY1rcO+7f2F6RVPlnEVpfmJhDEEByvVj3RM1dFERKhQWbFK4QXzRJEFGRF3srqY1Y\niRqPmUM+oX3yK+RmTDmRPqbRqECpHvYjYiwPAUzylEPrD+dGKpbt4FilwLyJCAeX\nTKvpWo3rTDsv4McL8nJ5bv1eZukroinGIPpK+LfW0QKBgQDXaP5/A5OBSPnnY8rJ\nZ74AaLibnTF921/DW8enIgoSCWXkP8Y00yneeiUKnVZqTTSN3Wt8Wzcs29ReM9PL\nfEMk1RvoPewablDLoFpGGWSTpiOPG24NudUJbK6U/yFYTiNTFsolEUhcMmyd0TRk\nrmKPeRs5o+fR//2M2awDp0oC8QKBgQC9hVlc1RewMXQ1nRyFb5gpMBZ9OxZUIBR1\nzfXvdBokE5FqwESpbwb3D1PCf5z+v6szZvZFbJ7IQH9GOXve++HH+OeVNKP7IU1X\n0cbd5ScAkAAGw3Ywt7avS4LYi0W2IOjtD/bwggVpvJlHoCqbqRcHHwefwdfKj8io\noN+Wr4cJUwKBgQCoSjXyA1g9PEdJeJRYj31IkogPw1qEKlyBovWzZIublc4N6akv\nYf4m+yRvS4bU2+aAIwmziQvtNv2Ob007f6RxZTE9WwtXhvIGdAuzEmmaVMUZK+Pq\nQ1Hl4AvxCfYCKGG+q2JYXU08AoULMwOM2RTVZFdZy+z0CtwLK5AQ9qSC8QKBgQCv\njyhxs0AHSjhxNcHm5bIZbV9X0HVlpnXtjsXkdDOG+c16CvNP1PapxDv970LaRWjs\n69L0CqOnsv9zaOqTa9cB7FNyRtokVvwTftLuWEjh34L2wPfqNzR2e8eQmqsQBLpg\nZ6qDrp4fDcMbs4XkviUDxr3EQseH79ucQbH9H0pC2wKBgFZjdJdiPcRE0rZpG2NB\n+4+m5Yk8BKtlNl6yrhEU7GNT+ePAAyZiTKw+B5Gik8RszmfnzaKdy8ZP0C+3jG2i\nhbZKQcLjcfedAfCWmS99HzlbB+jUagfmv8eiln+X+6M8fqWA8TdR8iMpOB7LnAyo\ntsbSiX2Sn8REc8Doyy8J2TS4\n-----END PRIVATE KEY-----\n",
-    "client_email": "dialogflow-ddslcj@solveit-onjhqn.iam.gserviceaccount.com",
+    "type": "...",
+    "project_id": "....",
+    "private_key_id": "......",
+    "private_key": "anl8w2+zD+0ApnZR5bfcqUi2Oi825\nICg9RhnxBuqgNVzd4jrLdpO3jgc2/VDszh...s.as..add.w...\nXizc//XGyTYn4U7T9Y0cpBa1XGpyBPXilHfeC7RBJeE0Ow1JhV2Xb3sTrCgA3gN1\nXlEjMG0jAgMBAAECggEACTbrCDTUOLU0Gj5kcNsutOmRgdC6TFkdDOrPbES0ev9Z\nYA4JZSHsY++QYBQ73Gpx8aNGCn7L+hjJ/wXhOIOfCIqDWXKI3p7KUQAX6uQphTbI\nW5dg4cQpQO6m7tNrljFWR8acXl7S5hEwSRmmvupNi8P4C7DPAGfW44nNqEYLJwNY\nY1rcO+7f2F6RVPlnEVpfmJhDEEByvVj3RM1dFERKhQWbFK4QXzRJEFGRF3srqY1Y\niRqPmUM+oX3yK+RmTDmRPqbRqECpHvYjYiwPAUzylEPrD+dGKpbt4FilwLyJCAeX\nTKvpWo3rTDsv4McL8nJ5bv1eZukroinGIPpK+LfW0QKBgQDXaP5/A5OBSPnnY8rJ\nZ74AaLibnTF921/DW8enIgoSCWXkP8Y00yneeiUKnVZqTTSN3Wt8Wzcs29ReM9PL\nfEMk1RvoPewablDLoFpGGWSTpiOPG24NudUJbK6U/yFYTiNTFsolEUhcMmyd0TRk\nrmKPeRs5o+fR//2M2awDp0oC8QKBgQC9hVlc1RewMXQ1nRyFb5gpMBZ9OxZUIBR1\nzfXvdBokE5FqwESpbwb3D1PCf5z+v6szZvZFbJ7IQH9GOXve++HH+OeVNKP7IU1X\n0cbd5ScAkAAGw3Ywt7avS4LYi0W2IOjtD/bwggVpvJlHoCqbqRcHHwefwdfKj8io\noN+Wr4cJUwKBgQCoSjXyA1g9PEdJeJRYj31IkogPw1qEKlyBovWzZIublc4N6akv\nYf4m+yRvS4bU2+aAIwmziQvtNv2Ob007f6RxZTE9WwtXhvIGdAuzEmmaVMUZK+Pq\nQ1Hl4AvxCfYCKGG+q2JYXU08AoULMwOM2RTVZFdZy+z0CtwLK5AQ9qSC8QKBgQCv\njyhxs0AHSjhxNcHm5bIZbV9X0HVlpnXtjsXkdDOG+c16CvNP1PapxDv970LaRWjs\n69L0CqOnsv9zaOqTa9cB7FNyRtokVvwTftLuWEjh34L2wPfqNzR2e8eQmqsQBLpg\nZ6qDrp4fDcMbs4XkviUDxr3EQseH79ucQbH9H0pC2wKBgFZjdJdiPcRE0rZpG2NB\n+4+m5Yk8BKtlNl6yrhEU7GNT+ePAAyZiTKw+B5Gik8RszmfnzaKdy8ZP0C+3jG2i\nhbZKQcLjcfedAfCWmS99HzlbB+jUagfmv8eiln+X+6M8fqWA8TdR8iMpOB7LnAyo\ntsbSiX2Sn8REc8Doyy8J2TS4\n-----END PRIVATE KEY-----\n",
+    "client_email": "....",
     "client_id": "104232866901480286869",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-ddslcj%40solveit-onjhqn.iam.gserviceaccount.com"
+    "auth_uri": "...",
+    "token_uri": "...",
+    "auth_provider_x509_cert_url": "....",
+    "client_x509_cert_url": "....."
   }
   
